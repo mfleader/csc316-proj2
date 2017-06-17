88ad0e6c6f7e1c8de0aaae560fc634cdeb6a7c6a
Matthew F. Leader <mfleader@ncsu.edu>
2017-06-17 17:14:03 -0400

    Add some body to main method of proj2 class

--- FamilyTrees/src/tree/TreeNode.java
+++ FamilyTrees/src/tree/TreeNode.java
@@ -1,5 +1,7 @@
 package tree;
 
+import java.util.LinkedList;
+
 public class TreeNode<E> {
 	
     /** the data within the element */
--- FamilyTrees/src/tree/proj2.java
+++ FamilyTrees/src/tree/proj2.java
@@ -1,9 +1,63 @@
 package tree;
 
+import java.util.Scanner;
+
 public class proj2 {
+	
+	private char[] pretrav;
+	private char[] posttrav;
 	
 	public static void main(String[] args) {
 		
+		
+        Scanner input = new Scanner(System.in);
+        while (input.hasNextLine()) {
+        	String line = input.nextLine();
+        	if (line.charAt(0) == '<') {
+        		
+        	} else if (line.charAt(0) == '>') {
+        		
+        	} else if (line.charAt(0) == '?') {
+        		
+        	}
+        }
+		
 	}
 
+
+	/**
+	 * 
+	 * @param size
+	 * 				the number of nodes in the subtree to be built
+	 * @param prestart
+	 * 				the place in pretrav where the preorder traversal of this subtree begins
+	 * @param poststart
+	 * 				the place in posttrav where the post order traversal of this subtree begins
+	 */
+	public static void buildTree(int size, int prestart, int poststart) {
+		
+	}
+	
+	public static char[] makePretrav(String line) {
+		char[] array = null;
+		int index = 0;
+		
+		if (line.length() > 0) {
+			array = new char[line.length()];
+			for (int k = 0; k < line.length(); k++) {
+				if (line.charAt(k) != ' '
+					&& line.charAt(k) != '<'
+					&& line.charAt(k) != '>'
+					&& line.charAt(k) != '?'
+					&& line.charAt(k) != '.'
+					&& line.charAt(k) != ',') {
+					
+					array[index++] = line.charAt(k);
+				}
+			}
+		}
+		
+		return array;
+	}
+	
 }
